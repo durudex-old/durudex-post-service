861d8f7df73ce327a4a88b5cae6b932aa8e3fdd7
V1def <vaniya8871@gmail.com>
2022-07-17 12:57:54 +0200

    Update postgres database schema

--- schema/000001_durudex.up.sql
+++ schema/000001_durudex.up.sql
@@ -1,26 +1,23 @@
 /*
  * Copyright © 2022 Durudex
-
+ *
  * This file is part of Durudex: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
-
+ *
  * Durudex is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
-
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with Durudex. If not, see <https://www.gnu.org/licenses/>.
  */
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE IF NOT EXISTS "post" (
-  "id"         UUID      NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-  "author_id"  UUID      NOT NULL,
+  "id"         CHAR(27)  NOT NULL PRIMARY KEY,
+  "author_id"  CHAR(27)  NOT NULL,
   "text"       TEXT      NOT NULL,
-  "created_at" TIMESTAMP NOT NULL DEFAULT now(),
   "updated_at" TIMESTAMP
 );
